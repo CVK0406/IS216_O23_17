327a76ba3c133c33596fe8c5c4b77f6aa2c50349
Khoi Le <minhkhoile1110@gmail.com>
2024-05-20 19:52:58 +0700

    Upload Models

---
+++ src/Model/KieuPhong.java
@@ -0,0 +1,52 @@
+package Model;
+
+public class KieuPhong {
+    private String MaLoaiPhg;
+    private String KieuPhong;
+    private int KieuGiuong;
+    private int DonGia;
+
+    public KieuPhong() {
+    }
+
+    public KieuPhong(String MaLoaiPhg, String KieuPhong, int KieuGiuong, int DonGia) {
+        this.MaLoaiPhg = MaLoaiPhg;
+        this.KieuPhong = KieuPhong;
+        this.KieuGiuong = KieuGiuong;
+        this.DonGia = DonGia;
+    }
+
+    public String getMaLoaiPhg() {
+        return MaLoaiPhg;
+    }
+
+    public void setMaLoaiPhg(String MaLoaiPhg) {
+        this.MaLoaiPhg = MaLoaiPhg;
+    }
+
+    public String getKieuPhong() {
+        return KieuPhong;
+    }
+
+    public void setKieuPhong(String KieuPhong) {
+        this.KieuPhong = KieuPhong;
+    }
+
+    public int getKieuGiuong() {
+        return KieuGiuong;
+    }
+
+    public void setKieuGiuong(int KieuGiuong) {
+        this.KieuGiuong = KieuGiuong;
+    }
+
+    public int getDonGia() {
+        return DonGia;
+    }
+
+    public void setDonGia(int DonGia) {
+        this.DonGia = DonGia;
+    }
+  
+  
+}
